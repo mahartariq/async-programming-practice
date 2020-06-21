08fbf66e09185553407c0691dc8b85dcd3b51a71
MaharTariq <mahartariq499@gmail.com>
2020-06-21 23:44:11 +0500

    sync pract

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("before Prepare Food");
+
+function prepareFood(){
+    setTimeout(()=>{
+        console.log("Prepare Food");
+    })
+}
+
+prepareFood();
+
+console.log("after Prepare Food");
