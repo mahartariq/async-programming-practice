7429449f7e611806fc9831782dfad3d8fc0de16a
MaharTariq <mahartariq499@gmail.com>
2020-06-22 00:31:39 +0500

    functions + callbacks, callback hell

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 console.log("before Prepare Food");
-
-function prepareFood(){
+function prepareFood(callback){
     setTimeout(()=>{
         console.log("Prepare Food");
-    })
+        callback("Food is ready");
+    },20);
 }
-
-prepareFood();
-
+function prepareFrenchToast(callback){
+    setTimeout(()=>{
+        console.log("Prepare French Toast");
+        callback("French Toast is Ready");
+    });
+}
+function mycallback(value){
+    console.log("Food Callback: ",value);
+    prepareFrenchToast(frenchToast);
+}
+function frenchToast(value){
+    console.log("French-Toast Callback:",value);
+    prepareCoffee(coffee);
+}
+function prepareCoffee(callback){
+    setTimeout(()=>{
+        console.log("Prepare Coffee");
+        callback("Coffee is Ready");
+    });
+}
+function coffee(value){
+    console.log("Coffee Callback:",value);
+}
+prepareFood(mycallback);
 console.log("after Prepare Food");
