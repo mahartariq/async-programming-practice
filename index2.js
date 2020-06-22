5834c24faee463ac693556b581a8b193b67c53e5
MaharTariq <mahartariq499@gmail.com>
2020-06-22 09:10:25 +0500

    promise made

---
+++ index2.js
@@ -0,0 +1,64 @@
+console.log("before Prepare Food");
+function prepareFood(data){
+    let promise = new Promise(function(resolve, reject){
+        setTimeout(()=>{
+            if (data!=2){
+                console.log("Prepare Food");
+                resolve("Food is ready");   
+            }
+            else{
+                reject("Something went wrong");
+            }
+
+        },20);
+    });
+    return promise;
+}
+function prepareFrenchToast(){
+    let promise = new Promise(function(resolve, reject){
+        setTimeout(()=>{
+            console.log("Prepare French Toast");
+            resolve("French Toast is Ready");
+        });    
+    });
+    return promise;
+}
+
+function prepareCoffee(){
+    let promise = new Promise(function(resolve,reject){
+        setTimeout(()=>{
+            console.log("Prepare Coffee");
+            resolve("Coffee is Ready");
+        });    
+    });
+    return promise;
+}
+
+let promise = prepareFood(2)
+//console.log("Promise:", promise);
+promise.then(function (value){
+    console.log("Food Callback: ",value);
+    return prepareFrenchToast();
+})
+.then(function(value){
+    console.log("French-Toast Callback:",value);
+    return prepareCoffee();
+})
+.then(function(value){
+    console.log("Coffee Callback:",value);
+})
+.catch(function(error){
+    console.log("Food error Callback",error)
+});
+/*
+prepareFood(function(value){
+    
+    prepareFrenchToast(function(value){
+    
+        prepareCoffee(function(value){
+    
+        });
+    });
+});
+*/
+console.log("after Prepare Food");
